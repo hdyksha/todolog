2902f8c53c4abfdab14a0418c700f24f9a644528
hdyksha <hideyuki.sha.11@gmail.com>
2025-04-18 10:14:33 +0900

    delte all files

--- src/types/TaskFile.ts
+++
@@ -1,9 +0,0 @@
-/**
- * タスクファイルの情報を表す型
- */
-export interface TaskFile {
-  name: string;
-  path: string;
-  size: number;
-  lastModified: string;
-}
--- src/types/TaskOperationResult.ts
+++
@@ -1,8 +0,0 @@
-/**
- * タスク操作の結果を表す型
- */
-export interface TaskOperationResult<T> {
-  success: boolean;
-  data?: T;
-  error?: string;
-}
